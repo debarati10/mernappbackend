a4a5a867939211ff10e28d91beffe2d640df84d3
debarati10 <debaratichakrabarti10@gmail.com>
2022-08-01 00:28:26 +0530

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/orders', orderRoutes);
 app.use('/images', imageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/frontend/build'));
+  app.use(express.static('./frontend/build'));
   //const path= require("path");
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
